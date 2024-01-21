0a93ba3a235006cd8d0f264b979fc2582a37b08d
Ashley Cox <ashleycox1@mail.weber.edu>
2024-01-20 19:42:16 -0700

    Expenses done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,5 @@
-// ExpenseItem is the name we access the import by and the from is where it's located sense it's a js file we don't need to put the .js at the end
-import ExpenseItem from "./components/ExpenseItem";
-
+// Expenses is the name we access the import by and the from is where it's located sense it's a js file we don't need to put the .js at the end
+import Expenses from "./components/Expenses";
 function App() {
   // Creating an array (we use this to create multiple peices of data)
   const expenses = [
@@ -24,7 +23,6 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-
   // JavaScript way to display to website
   // const para = document.createElement("p");
   // para.textContent = "This is also visible";
@@ -36,32 +34,8 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      {/* <p>This is visible</p> */}
+      <Expenses items={expenses}/>
 
-      {/*JSX syntax to call component  */}
-      {/* this way */}
-      {/* <ExpenseItem /> */}
-      {/* or this way */}
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
     </div>
   );
 }
